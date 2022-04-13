658544d1505517f93bca1f3b9b7e7b925103fa78
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2022-04-13 18:41:35 -0500

    Daily kata

--- javascriptLog.js
+++ javascriptLog.js
@@ -338,5 +338,41 @@ function pigIt(str) {
 	return pigLatin.join(' ')
 }
 
-console.log(pigIt('Pig latin is cool'))
-console.log(pigIt('This is my string !'))
+// console.log(pigIt('Pig latin is cool'))
+// console.log(pigIt('This is my string !'))
+
+
+// Day: 4/13/22
+
+// Today's Progress:
+
+// Thoughts:
+
+// Link(s) to work: https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+
+
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+function descendingOrder(n) {
+	return parseInt(n
+		.toString()
+		.split('')
+		.sort((a, b) => b - a)
+		.join('')
+	)
+}
+
+
+
+console.log(descendingOrder(0))
+console.log(descendingOrder(1))
+console.log(descendingOrder(111))
+console.log(descendingOrder(15))
+console.log(descendingOrder(1021))
+console.log(descendingOrder(123456789))
